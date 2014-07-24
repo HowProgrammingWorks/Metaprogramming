d88903d1c62fc7164895ead79be7535de8f3bb13
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2014-07-24 11:47:04 +0300

    Added factory to build classes dynamically

---
+++ 4-class-factory/class-inheritance-metaprogramming.js
@@ -0,0 +1,35 @@
+﻿// Base class
+//
+var Ground = function(area) {
+	this.area = area;
+};
+
+// Base class prototype method
+//
+Ground.prototype.calculateCost = function(price) {
+	return this.area * price;
+}
+
+// MetaFactory to build descendant classes
+//
+function inheritGround(mixin) {
+
+	var iGround = function(area) {
+		this.area = area;
+		for (var property in mixin) this[property] = mixin[property];
+	};
+
+	iGround.prototype = Object.create(Ground.prototype);
+	return iGround;
+
+}
+
+// Create descendant class dynamically
+//
+var LandOwnership = inheritGround({ category:"land", type:"ownership" });
+
+// Create and use instance
+//
+var land = new LandOwnership(50);
+console.dir(land);
+console.log('Cost is: '+land.calculateCost(7));
---
+++ 4-class-factory/class-inheritance-simple.js
@@ -0,0 +1,29 @@
+﻿// Base class
+//
+var Ground = function(area) {
+	this.area = area;
+};
+
+// Base class prototype method
+//
+Ground.prototype.calculateCost = function(price) {
+	return this.area * price;
+}
+
+// Create descendant class
+//
+var LandOwnership = function(area) {
+	this.area = area;
+	this.category = "land";
+	this.type = "ownership";
+};
+
+// Use protorype inheritance from Ground
+//
+LandOwnership.prototype = Object.create(Ground.prototype);
+
+// Create and use instance
+//
+var land = new LandOwnership(50);
+console.dir(land);
+console.log('Cost is: '+land.calculateCost(7));
